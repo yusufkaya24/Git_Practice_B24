09d0c3f450c16b5d887e31e227695d6ff80c8a49
Yusuf Kaya <ysfky24@gmail.com>
2021-10-03 15:37:58 -0400

    made car in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,11 @@
+package setup;
+
+public class Car {
+
+    // this is car class from master branch
+
+    // we will have conflict
+
+    // because there is different text in different branches for car
+
+}
